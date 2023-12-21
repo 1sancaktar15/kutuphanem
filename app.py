6e306a315354e30c002c585fdc70a40cbc28f54a
elif özün <ozunelif0@gmail.com>
2023-12-21 20:56:40 +0300

    navigation bar ekleme ve jinja template kullanımı

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template("home.html")
+
+
+@app.route("/books")
+def kitap_page():
+    items = [
+    {'id': 1, 'name': 'Gece Yarısı Kütüphanesi', 'genre': 'Fantastik Kurgu', 'page': 283},
+    {'id': 2, 'name': 'Zafer Sızlanarak Kazanılmaz', 'genre': 'Kişisel Gelişim', 'page': 480},
+    {'id': 3, 'name': 'Eğitilmemiş Zihin', 'genre': 'Psikoloji', 'page': 324}
+    ]
+    return render_template("books.html", items = items)
 
